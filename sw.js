d8f68e9f122c4bf861032efd62e54d52ed18ad5f
Mahdi Alizadeh <mahdi.alizadeh.sp@gmail.com>
2023-07-28 21:02:15 +0330

    add service worker

---
+++ sw.js
@@ -0,0 +1,50 @@
+
+self.addEventListener('install', async (event) => {
+
+    console.log('[Service Worker] Installing Service Worker ...');
+
+    const resources = ['/', '/index.html', '/style.css', '/script/dots.js', '/script/script.js', '/media/patt.png', '/media/icon.png', '/media/math2.png'];
+
+    event.waitUntil(addResourcesToCache(resources));
+
+});
+
+self.addEventListener('activate', (event) => {
+
+    console.log('[Service Worker] Activating Service Worker ...');
+
+});
+
+self.addEventListener('fetch', async (event) => {
+
+    console.log('[Service Worker] Fetching From Cache ...');
+
+    await event.respondWith(matchResourcesInFetch(event.request));
+
+});
+
+// Add Resources To Cache --->
+async function addResourcesToCache (resources) {
+
+    const cache = await caches.open("v1");
+
+    await cache.addAll(resources);
+
+};
+
+// Match Resources in Fetch --->
+async function matchResourcesInFetch (request) {
+
+    const response = await caches.match(request);
+
+    if (response) {
+
+        return response;
+
+    } else {
+
+        return fetch(request);
+
+    }
+
+};
